a701e747ec6d554cb9eda7a4f49660c8d978c447
marylyne <otienomarylyne76@gmail.com>
2025-05-12 12:29:37 +0300

    adds dog.py person.py

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name="Dog"):
+        self.name = name
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
+
+
+
     # Class body goes here
 
     #Instance method definition
-    pass
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+
+
+class Person:
+    def __init__(self, name="Ajib"):
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
